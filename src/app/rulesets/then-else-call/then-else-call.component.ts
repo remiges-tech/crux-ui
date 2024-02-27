1850f234826cc48f41823892cec227c748ee943e
sufiyaans <sufiyaan.shaik@merce.co>
2024-02-27 18:17:48 +0530

    refs: redmine #17955 Minor changes in workflow details UI Screen

--- src/app/rulesets/then-else-call/then-else-call.component.ts
+++ src/app/rulesets/then-else-call/then-else-call.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { RTree, RulePatternTerm } from 'src/models/common-interfaces';
 import { CommonService } from 'src/services/common.service';
 import { OperatorsUnicode } from 'src/services/constants.service';
+import { RuleModalComponent } from '../rule-modal/rule-modal.component';
 
 @Component({
   selector: 'app-then-else-call',
@@ -13,6 +15,15 @@ export class ThenElseCallComponent {
   private _commonService = inject(CommonService);
   OperatorsUnicode: any = OperatorsUnicode;
   @Input() parentHovered: boolean = false;
+
+  constructor(private dialog: MatDialog) { }
+
+  openRuleModal(): void {
+    const dialogRef = this.dialog.open(RuleModalComponent, {
+      width: '80%',
+      data: { Rule: this.Rule }
+    });
+  }
  
 
   toggleParentHover(state: boolean) {
